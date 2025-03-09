aab879b7fb6eb9f86004596c27940de36de4c3a9
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-03-09 21:30:22 +0200

    update post creation view to dynamically populate creator options from users

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -29,8 +29,9 @@
                         <select name="post_creator" id="creator" required
                             class="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-2 px-3 border bg-white">
                             <option value="" disabled selected>Select a creator</option>
-                            <option value="1">Ahmed</option>
-                            <option value="2">Mirage</option>
+                            @foreach ($users as $user)
+                                <option value="{{ $user->id }}">{{ $user->name }}</option>
+                            @endforeach
                         </select>
                     </div>
 
