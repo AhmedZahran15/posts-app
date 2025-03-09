326ecc5c21f19422d5909fe61cc4f3334af0a9ae
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-03-09 22:06:03 +0200

    add nested routes for comments under posts

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\PostController;
+use \App\Http\Controllers\CommentController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Posts routes
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
@@ -13,3 +16,11 @@ Route::delete('/posts/{post}/delete', [PostController::class, 'destroy'])->name(
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+// Nested routes for comments
+Route::resource('posts.comments', CommentController::class)->only([
+    'store',
+    'edit',
+    'update',
+    'destroy'
+]);
