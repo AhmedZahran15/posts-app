0071483deff2b9c788e83f2004702a6963bc84b0
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-03-14 17:47:19 +0200

    refactor: update app.js to implement Inertia.js for improved page handling and component resolution

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,40 +1,26 @@
-import { createApp } from 'vue';
 import '../css/app.css';
 import './bootstrap';
 
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-// Create a standard Vue application
-import ViewAjax from './components/ViewAjax.vue';
-createApp({
-    components: {
-        ViewAjax,
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
     },
-    setup() {
-        return {};
+    progress: {
+        color: '#4B5563',
     },
-})
-    .component('ViewAjax', ViewAjax)
-    .mount('#app');
-
-// createInertiaApp({
-//     title: (title) => `${title} - ${appName}`,
-//     resolve: (name) =>
-//         resolvePageComponent(
-//             `./Pages/${name}.vue`,
-//             import.meta.glob('./Pages/**/*.vue'),
-//         ),
-//     setup({ el, App, props, plugin }) {
-//         const app = createApp({ render: () => h(App, props) })
-//             .use(plugin)
-//             .use(ZiggyVue);
-
-//         // Register the component globally
-//         app.component('ViewAjax', ViewAjax);
-
-//         app.mount(el);
-//     },
-//     progress: {
-//         color: '#4B5563',
-//     },
-// });
+});
