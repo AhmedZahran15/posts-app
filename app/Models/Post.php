e97b1bc2ee8f90729c96a12a5ede3a65cd118576
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-03-15 05:35:24 +0200

    feat: enable slug update on Post model when title changes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,7 +22,8 @@ class Post extends Model
     {
         return [
             'slug' => [
-                'source' => 'title'
+                'source' => 'title',
+                'onUpdate' => true,
             ]
         ];
     }
