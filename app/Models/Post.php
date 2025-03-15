e014e66a640652e13260a831b7d33f0c6042111c
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2025-03-15 05:28:36 +0200

    feat: integrate sluggable functionality in Post model for automatic slug generation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,16 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
 
     protected $fillable = [
         'title',
         'description',
         'user_id',
+        'slug',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 
     public function user()
     {
